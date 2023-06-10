01dfac9dd5933e64f60a8ad5ec13dd8ffed2fb95
Jonghun Park - 朴 鍾勲 <devtopia@icloud.com>
2023-06-10 21:58:55 +0900

    Fix jest error in the mdx file

--- jest.config.cjs
+++ jest.config.cjs
@@ -5,11 +5,7 @@ module.exports = {
     "<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}",
     "<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}",
   ],
-  testPathIgnorePatterns: [
-    "<rootDir>/dist",
-    "<rootDir>/node_modules",
-    "<rootDir>/src/coniot_common_frontend",
-  ],
+  testPathIgnorePatterns: ["<rootDir>/dist", "<rootDir>/node_modules"],
   modulePathIgnorePatterns: [
     "<rootDir>/(build|docs|node_modules|scripts|dist)",
   ],
@@ -18,7 +14,7 @@ module.exports = {
   },
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": "esbuild-jest",
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|mdx)$":
       "<rootDir>/config/fileTransformer.js",
   },
   setupFilesAfterEnv: ["<rootDir>/config/jest.js"],
